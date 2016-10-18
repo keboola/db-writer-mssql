0666e95589e19bda7076dcb8d166c9a966142939
Erik Zigo <erik.zigo@keboola.com>
2016-10-18 10:26:59 +0200

    change ident from tabs to spaces

--- tests/Keboola/DbWriter/Writer/MSSQLEntrypointTest.php
+++ tests/Keboola/DbWriter/Writer/MSSQLEntrypointTest.php
@@ -6,31 +6,31 @@ use Symfony\Component\Yaml\Yaml;
 
 class MSSQLEntrypointTest extends BaseTest
 {
-	const DRIVER = 'mssql';
+    const DRIVER = 'mssql';
 
-	/** @var MSSQL */
-	private $writer;
+    /** @var MSSQL */
+    private $writer;
 
-	private $config;
+    private $config;
 
-	public function testRunAction()
-	{
-		$rootPath = __DIR__ . '/../../../../';
+    public function testRunAction()
+    {
+        $rootPath = __DIR__ . '/../../../../';
 
-		$this->config = Yaml::parse(file_get_contents($rootPath . 'tests/data/runAction/config.yml'));
-		$this->config['parameters']['writer_class'] = 'MSSQL';
+        $this->config = Yaml::parse(file_get_contents($rootPath . 'tests/data/runAction/config.yml'));
+        $this->config['parameters']['writer_class'] = 'MSSQL';
 
-		$this->writer = $this->getWriter($this->config['parameters']);
+        $this->writer = $this->getWriter($this->config['parameters']);
 
-		// cleanup
-		foreach ($this->config['parameters']['tables'] AS $table) {
-			$this->writer->drop($table['dbName']);
-		}
+        // cleanup
+        foreach ($this->config['parameters']['tables'] AS $table) {
+            $this->writer->drop($table['dbName']);
+        }
 
-		// run entrypoint
-		$lastOutput = exec('php ' . $rootPath . 'run.php --data=' . $rootPath . 'tests/data/runAction 2>&1', $output, $returnCode);
+        // run entrypoint
+        $lastOutput = exec('php ' . $rootPath . 'run.php --data=' . $rootPath . 'tests/data/runAction 2>&1', $output, $returnCode);
 
-		$this->assertEquals(0, $returnCode);
+        $this->assertEquals(0, $returnCode);
 
-	}
+    }
 }
