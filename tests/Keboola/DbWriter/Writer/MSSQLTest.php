7841485ae70afcb7a2501958232f72875e133e26
Erik Zigo <erik.zigo@keboola.com>
2016-10-19 13:37:02 +0200

    tests: removed writeSingleLine added reorderRenameIgnoreColumns

--- tests/Keboola/DbWriter/Writer/MSSQLTest.php
+++ tests/Keboola/DbWriter/Writer/MSSQLTest.php
@@ -186,32 +186,91 @@ class MSSQLTest extends BaseTest
         ], $allowedTypes);
     }
 
-    public function testWriteSingleLine()
+    public function testReorderRenameIgnoreColumns()
     {
+        $conn = $this->writer->getConnection();
         $tables = $this->config['parameters']['tables'];
 
-        // simple table
         $table = $tables[0];
-        $sourceTableId = $table['tableId'];
-        $outputTableName = $table['dbName'];
-        $sourceFilename = $this->dataDir . "/" . $sourceTableId . "_single.csv";
 
-        $this->writer->drop($outputTableName);
+        // reorder
+        $table['items'] = array_reverse($table['items']);
+
+        // rename
+        foreach ($table['items'] AS $key => $column) {
+            $table['items'][$key]['dbName'] = md5($column['dbName']);
+        }
+        foreach ($table['primaryKey'] AS $key => $column) {
+            $table['primaryKey'][$key] = md5($column);
+        }
+
+        // ignore
+        foreach ($table['items'] AS $key => $column) {
+            if ($column['name'] === 'glasses') {
+                $table['items'][$key]['type'] = 'IGNORE';
+            }
+        }
+
+        $sourceFilename = $this->dataDir . "/" . $table['tableId'] . ".csv";
+        $targetTable = $table;
+        $table['dbName'] .= $table['incremental']?'_temp_' . uniqid():'';
+
+        // first write
+        $this->writer->create($targetTable);
+        $this->writer->write(new CsvFile($sourceFilename), $targetTable);
+
+        // second write
+        $sourceFilename = $this->dataDir . "/" . $table['tableId'] . "_increment.csv";
+
         $this->writer->create($table);
-        $this->writer->write(new CsvFile(realpath($sourceFilename)), $table);
+        $this->writer->write(new CsvFile($sourceFilename), $table);
+        $this->writer->upsert($table, $targetTable['dbName']);
 
-        $conn = $this->writer->getConnection();
-        $stmt = $conn->query("SELECT * FROM $outputTableName");
+
+        $expectedFilename = $this->dataDir . "/" . $table['tableId'] . "_merged.csv";
+        $expectedCsv = new CsvFile($expectedFilename);
+
+        // prepare validation file
+        $expectedHeaderMap = array();
+        foreach ($table['items'] AS $column) {
+            if ($column['type'] === 'IGNORE') {
+                continue;
+            }
+
+            $expectedHeaderMap[$column['name']] = $column['dbName'];
+        }
+
+        $tmpExpectedFilename = tempnam('/tmp', md5($expectedFilename));
+        $tmpExpectedCsv = new CsvFile($tmpExpectedFilename);
+
+        $header = $expectedCsv->getHeader();
+        foreach ($expectedCsv AS $i => $row) {
+            if (!$i) {
+                $tmpExpectedCsv->writeRow($expectedHeaderMap);
+                continue;
+            }
+
+            $newRow = [];
+
+            $row = array_combine($header, $row);
+            foreach ($expectedHeaderMap AS $originName => $newName) {
+                $newRow[$newName] = $row[$originName];
+            }
+
+            $tmpExpectedCsv->writeRow($newRow);
+        }
+
+        $stmt = $conn->query("SELECT " . implode(', ', $expectedHeaderMap) . " FROM {$targetTable['dbName']}");
         $res = $stmt->fetchAll(\PDO::FETCH_ASSOC);
 
         $resFilename = tempnam('/tmp', 'db-wr-test-tmp');
         $csv = new CsvFile($resFilename);
-        $csv->writeRow(["id","name","glasses"]);
+        $csv->writeRow($expectedHeaderMap);
         foreach ($res as $row) {
             $csv->writeRow($row);
         }
 
-        $this->assertFileEquals($sourceFilename, $resFilename);
+        $this->assertFileEquals($tmpExpectedFilename, $resFilename);
     }
 
     public function testUpsert()
