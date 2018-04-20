31f94dad714d12f4e58c610fe2a3c9e302a0d33a
Miroslav Čillík <miro@keboola.com>
2018-04-20 13:25:18 +0200

    refactor: updatet image to the latest php 7.2

--- phpunit.sh
+++ phpunit.sh
@@ -43,5 +43,4 @@ dockerize -wait tcp://mssql:1433
 sleep 20
 
 ./vendor/bin/phpcs --standard=psr2 -n --ignore=vendor --extensions=php . \
-  && ./vendor/bin/phpunit "$@" \
-  && ./vendor/bin/test-reporter
+  && ./vendor/bin/phpunit "$@"
